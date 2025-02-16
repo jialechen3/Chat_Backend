c29042a342e43664839b5fea81c85c030e194869
jialechen3 <jiale1810418002@gmail.com>
2025-02-16 16:30:30 -0500

    add server

--- server.py
+++ server.py
@@ -1,4 +1,11 @@
 import socketserver
+
+from util.chat_create import chat_create
+from util.chat_delete import chat_delete
+from util.chat_get import chat_get
+from util.chat_update import chat_update
+from util.extractor import extractor
+from util.render import render
 from util.request import Request
 from util.router import Router
 from util.hello_path import hello_path
@@ -10,6 +17,14 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
         self.router = Router()
         self.router.add_route("GET", "/hello", hello_path, True)
         # TODO: Add your routes here
+
+        self.router.add_route("GET", "/public", extractor, False)
+        self.router.add_route("GET", "/", render, True)
+        self.router.add_route("GET", "/chat", render, True)
+        self.router.add_route("POST", "/api/chats", chat_create, True)
+        self.router.add_route("GET", "/api/chats", chat_get, True)
+        self.router.add_route("PATCH", "/api/chats", chat_update, False)
+        self.router.add_route("DELETE", "/api/chats", chat_delete, False)
         super().__init__(request, client_address, server)
 
     def handle(self):
